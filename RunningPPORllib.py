c7c47d11715f54019ba99acff87a0767ea80b1fd
anthonyahockman <anthonyahockman@gmail.com>
2023-03-23 15:27:05 -0400

    Attempt at a PPO Implement

---
+++ RunningPPORllib.py
@@ -0,0 +1,44 @@
+import ray
+import gymnasium as gym
+from gym import spaces
+# from gym import spaces
+from ray.rllib.algorithms.ppo import PPOConfig
+from ray.rllib.algorithms.callbacks import MemoryTrackingCallbacks
+from ray import tune
+from ray.tune.registry import register_env
+from ray.tune.logger import pretty_print
+from ray import air
+from envs.docking import SpacecraftDockingContinuous
+import logging 
+'''
+def environment():
+
+def training():
+
+def rollouts():
+
+def exploration():
+'''
+# num_rollout_workers=2, num_envs_per_worker=1 -- Can include these in .rollouts but idk how these work yet
+'''
+def env_creator(env_name):
+    from envs.docking import SpacecraftDockingContinuous as env
+    return env
+'''
+# env = env_creator()
+
+# ray.tune.registry.register_env('ARPOD_PPO', lambda: config, env(config))
+
+env = SpacecraftDockingContinuous()
+register_env('spacecraft-docking-continuous-v0', lambda config: env)
+config = PPOConfig().training(gamma = 0.9, lr=0.01)\
+    .environment("spacecraft-docking-continuous-v0")\
+    .resources(num_gpus=0)\
+    .rollouts(create_env_on_local_worker=True)\
+    .callbacks(MemoryTrackingCallbacks)
+
+pretty_print(config.to_dict())
+
+algo = config.build()
+
+algo.train()
